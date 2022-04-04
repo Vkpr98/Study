c381721f4ffe048e0d5f3d707b8a64ba918ad29b
йуйцу Кйцуйц <kapriorvadym@gmail.com>
2022-04-04 14:41:05 +0300

    Refactoring

---
+++ Js/index.js
@@ -0,0 +1,14 @@
+//document.querySelector(".selector") - повертає перший елемент який відповідає селектору переданому в аргумент
+//document.querySelectorAll(".selector") - повертає масив елементів які відповідає селектору переданому в аргумент
+//(будьякий елемент).innerHtml - міняє html елемента
+//(будьякий елемент).append - додає до елемента
+//document.createElement("div") - повертає новостворений елемент типу який переданий в якості парамету
+
+  document.getElementById("mainTable__header2").innerHTML="Стугна ";
+
+
+  document.getElementById("mainTable__header1").innerHTML="Стугна (ПТУР)";
+
+
+  document.getElementsByClassName("mainTable__image")[0].style.width = "900px";
+
